56b8ccf89cccaf426c57a2dc2c7c84ebde479e62
jaswanthnaiduputta777-a11y <jaswanthnaiduputta777@gmail.com>
2026-09-22 12:50:53 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first GitHub project.");
+        System.out.println("I'm Jashu, learning Java!");
+    }
+}
